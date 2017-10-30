dead21797c5f42634a33b4822843dd9f7329a527
vagrant <vagrant@homestead>
2017-10-30 20:56:50 +0000

    mili

---
+++ app/Http/CoursesController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Course;
+
+//use App\Http\Requests\CreatePostRequest;
+
+//use App\Http\Requests\UpdatePostRequest;
+
+use Illuminate\Http\Request;
+
+class CoursesController extends Controller
+{
+    
+	public function index( ) 
+	{
+		$courses = Course::with( 'courses' )->orderBy( 'id', 'desc' )->paginate( 10 );
+		return view( 'courses.index' )->with( [ 'courses' => $courses ] );
+	}
+
+	public function show( Course $course) 
+	{
+		return view( 'courses.show' );
+	}
+
+	public function create( ) 
+	{
+		return view( 'courses.create' );
+	}
+
+	public function store(  ) // CreateCourseRequest $request
+	{
+
+		return view( 'courses.show' );
+	}
+
+	public function edit( Course $course )
+	{
+
+		return view( 'courses.edit' );
+	}
+
+	public function update( Course $course ) // UpdateCourseRequest $request 
+	{
+		
+		return view( 'courses.show' );
+	}
+
+	public function delete( Course $course )
+	{
+		return view( 'courses.index' );
+	}
+}
